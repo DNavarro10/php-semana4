bac023931b3e43747beaf2547b321196fbb177c8
DNavarro10 <luis_d_100@hotmail.com>
2017-07-03 17:17:47 -0500

    Base de la semana pasada
    etc

---
+++ php/figura.php
@@ -0,0 +1,14 @@
+<?php 
+	abstract class figura{
+	
+		
+		abstract function area();
+		
+		abstract function perimetro();
+		
+		
+	}
+	define("Radio",40);
+	define("PI","3.1417");
+
+?>
---
+++ php/resultado.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resultado PHP</title>
+</head>
+<body>
+	Hola mundo
+</body>
+</html>
+
+
+?php 
+	include	'index.html';
+
+
+?>
